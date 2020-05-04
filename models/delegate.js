71078292a443b9dd6408d1fa0903a7e7d1ceb2bf
Benson Imoh,ST <benson@stbensonimoh.com>
2020-05-04 20:41:12 +0100

    feat: add mongoose model

---
+++ models/delegate.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const delegateSchema = new Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    phone: { type: String, required: true, trim: true },
+    city: { type: String, required: true, trim: true },
+    country: { type: String, required: true, trim: true }
+  },
+  { timestamps: true }
+)
+
+const Delegate = mongoose.model('Delegate', delegateSchema)
+
+module.exports(Delegate)
